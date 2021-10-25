ecdbe2226a84e9dc59f77e859df1031f762c43e9
Andre Santos <andresantosas2558487@gmail.com>
2021-10-25 20:45:37 -0300

    first commit for a teste

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-view />
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+</style>
